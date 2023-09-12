bf48830ea669651e8b38ed2c435839fece17a460
DivyaGhate <96178182+DivyaGhate@users.noreply.github.com>
2023-09-12 20:10:12 +0530

    cucumber project

---
+++ Day17/cucumberProject/src/test/java/com/stepDefination/Hooks.java
@@ -0,0 +1,14 @@
+package com.stepDefination;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+
+public class Hooks {
+	@Before
+	public void before() {
+		System.out.println("this is global before");
+	}
+	@After
+	public void after() {
+		System.out.println("This is global After");
+}}
